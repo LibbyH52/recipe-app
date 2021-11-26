25633b03f655b2c279d2e51588cb93155e7df89a
LibbyH <anne-elizabeth.henry@pwc.com>
2021-11-26 19:26:59 +0000

    Created routes in App. Added links to Navbar. Link also added to recipe cards, so can display recipe information when click on a recipe card

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React, {useState, useEffect } from 'react';
+import React, {useState, useEffect, Fragment } from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import axios from 'axios';
+import About from './components/About';
 import Recipes from './components/Recipes';
+import SingleRecipe from './components/SingleRecipe';
 import Search from './components/Search';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
@@ -10,21 +14,43 @@ function App() {
 
   useEffect(() => {
     const fetchRecipes = async() => {
-      const results = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&limitLicense=true&maxReadyTime=59&number=12&sortDirection='desc'`);
-      setRecipes(results.data.results);
-      console.log(results.data.results);
+      const results = await axios.get(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&limitLicense=true&maxReadyTime=59&number=12&sortDirection='desc'`);
+      setRecipes(results.data.recipes);
     }
     fetchRecipes();
   }, [])
 
+  const searchRecipes = (recipeName) => {
+    const getRecipes = async() => {
+    const results = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&query=${recipeName}&limitLicence=true&maxReadyTime=59&number=12`)
+    setRecipes(results.data.results);
+    }
+    getRecipes();
+  }
+
+  const getSingleRecipe = (id) => {
+    console.log(id);
+  }
+
   return (
-    <div className="App">
-      <Navbar />
-      <div className="main-container">
-          <Search />
-          <Recipes recipes={recipes}/>
+    <Router>
+        <Navbar />
+        <div className="main-container">
+        <Routes>
+          <Route exact path='/' element={
+              <About />
+          }/>
+          <Route exact path='/Recipes' element={
+            <div className="main-content">
+                <Search searchRecipes={searchRecipes}/>
+                <Recipes recipes={recipes} />
+            </div>
+          } />
+           <Route exact path='/SingleRecipe' element={<SingleRecipe/>} />
+        </Routes>
+        <Footer />
       </div>
-    </div>
+    </Router>
   );
 }
 
---
+++ src/components/SingleRecipe.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const SingleRecipe = () => {
+  return (
+    <div>
+      <h1>Recipe Name goes here</h1>
+    </div>
+  )
+}
+export default SingleRecipe;
