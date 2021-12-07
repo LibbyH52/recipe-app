e575c626118a1ff9d01583d5223d233076151be5
LibbyH <anne-elizabeth.henry@pwc.com>
2021-12-07 16:29:07 +0000

    Font awesome cdn added to index, additional components imported to App. Styled recipe-container

--- src/App.js
+++ src/App.js
@@ -7,16 +7,19 @@ import SingleRecipe from './components/SingleRecipe';
 import Search from './components/Search';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import RecipeState from './context/recipe/RecipeState';
 import './App.css';
 
 function App() {
   const [recipes, setRecipes] = useState([]);
   const [recipe, setRecipe] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchRecipes = async() => {
       const results = await axios.get(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&limitLicense=true&maxReadyTime=59&number=12&sortDirection='desc'`);
       setRecipes(results.data.recipes);
+      setIsLoading(false);
     }
     fetchRecipes();
   }, [])
@@ -33,34 +36,36 @@ function App() {
     const results = await axios.get(`https://api.spoonacular.com/recipes/${id}/information?apiKey=${process.env.REACT_APP_API_KEY}&includeNutrition=false`)
     //setRecipe(results);
     setRecipe(results.data);
-    console.log(results.data);
     }
 
   return (
-    <Router>
-        <Navbar />
-        <Routes>
-          <Route exact path='/' element={
-              <About />
-          }/>
-          <Route exact path='/Recipes' element={
-            <div className="content-container">
-                <Search searchRecipes={searchRecipes}/>
-                <Recipes recipes={recipes} />
-            </div>
-          } />
-        <Route 
-            exact path='/SingleRecipe/:id' 
-            element={
-              <SingleRecipe 
-                getRecipe={getRecipe} 
-                recipe={recipe}
-              />
-              }
-          />
-        </Routes>
-      <Footer />
-    </Router>
+    <RecipeState>
+      <Router>
+          <Navbar />
+          <Routes>
+            <Route exact path='/' element={
+                <About />
+            }/>
+            <Route exact path='/Recipes' element={
+              <div className="content-container">
+                  { isLoading && <div>Loading...</div> }   
+                  <Search searchRecipes={searchRecipes}/>
+                  <Recipes recipes={recipes} />        
+              </div>
+            } />
+          <Route 
+              exact path='/SingleRecipe/:id' 
+              element={
+                <SingleRecipe 
+                  getRecipe={getRecipe} 
+                  recipe={recipe}
+                />
+                }
+            />
+          </Routes>
+        <Footer />
+      </Router>
+    </RecipeState>
   );
 }
 
