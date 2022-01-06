8c75d514d0490f9ea8eae3b1b89bc728e20589d2
LibbyH <anne-elizabeth.henry@pwc.com>
2022-01-06 12:45:34 +0000

    Tidied up layout of code. Center-aligned recipe time on card

--- src/components/RecipeItem.js
+++ src/components/RecipeItem.js
@@ -5,35 +5,27 @@ import PropTypes from 'prop-types'
 import './RecipeItem.css'
 
 const RecipeItem = ({recipe: {id, image, title, readyInMinutes}, clock}) => {
-<<<<<<< HEAD
-  
-=======
 
->>>>>>> 504d1b8a8825d8daa1cdd32081f6bf2acb3f3a10
   return (
     <Link to={`/SingleRecipe/${id}`}>
-    <div className="recipe-card">
-      <div className="img-container">
-      {image ? 
-        <img src={image} alt={title} className="recipe-img" />
-        : <h5 className="missing-img">{title}</h5>
-      }
+      <div className="recipe-card">
+        <div className="img-container">
+          {image ? 
+            <img src={image} alt={title} className="recipe-img" />
+            : <h5 className="missing-img">{title}</h5>
+          }
+        </div>
+        <div className="title-container">
+          <h4 className="recipe-title">{title}</h4>
+          <p className="ready-time">
+            <span className="icon">
+                <i className={clock} />
+            </span>
+            {readyInMinutes ? `${readyInMinutes} minutes` :
+            <span>less than 1 hour</span>}
+          </p>
+        </div>
       </div>
-      <div className="title-container">
-        <h4 className="recipe-title">{title}</h4>
-        <p className="ready-time">
-          <span className="icon">
-              <i className={clock} />
-          </span>
-<<<<<<< HEAD
-           {readyInMinutes ? `${readyInMinutes} minutes` :
-           <span>less than 1 hour</span>}
-=======
-           {readyInMinutes} minutes
->>>>>>> 504d1b8a8825d8daa1cdd32081f6bf2acb3f3a10
-        </p>
-      </div>
-    </div>
     </Link>
   )
 }
