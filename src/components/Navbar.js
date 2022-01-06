29bcb8a1246b53e5ad274dd85e29a40b7ca94007
LibbyH <anne-elizabeth.henry@pwc.com>
2022-01-06 12:42:24 +0000

    Link added to recipe page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,4 @@
 import React from 'react'
-<<<<<<< HEAD
 import {Link, useNavigate} from 'react-router-dom'
 import './Navbar.css'
 
@@ -17,18 +16,6 @@ const Navbar = () =>  {
         <li className="nav-item" onClick={()=> navigate('/Recipes')}>
           <Link to='/Recipes'>Recipes</Link>
         </li>
-=======
-import {Link} from 'react-router-dom'
-import './Navbar.css'
-
-const Navbar = () =>  {
-  return (
-    <nav className="navbar">
-      <h1 className="main-heading"> The Impatient Chef</h1>
-      <ul className="nav-list">
-        <li className="nav-item"><Link to='/'>About</Link></li>
-        <li className="nav-item"><Link to='/Recipes'>Recipes</Link></li>
->>>>>>> 504d1b8a8825d8daa1cdd32081f6bf2acb3f3a10
       </ul>
     </nav>
   )
