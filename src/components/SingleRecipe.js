ac3e0787208ca4ac24079da45a8a291aaf76127b
LibbyH <anne-elizabeth.henry@pwc.com>
2022-01-06 10:05:15 +0000

    Imported useFragment, to use a wrapper div, pulled in information about diets, cuisine, and dish-types from api. Class names added to relevant divs

--- src/components/SingleRecipe.js
+++ src/components/SingleRecipe.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, Fragment} from 'react';
 import {useParams, useNavigate} from 'react-router-dom';
 import './SingleRecipe.css'
 
@@ -21,22 +21,13 @@ const SingleRecipe = ({getRecipe, recipe}) => {
         sourceName,
         readyInMinutes,
         servings,
+        dishTypes,
+        cuisines,
+        diets,
         analyzedInstructions,
         nutrition
       } = recipe;
-     
-      let nutrients = nutrition.nutrients;
-      const nutArr = [];
-
-      for(let i=0; i<nutrients.length; i++){
-        if(nutrients[i].name === 'Calories' || nutrients[i].name === 'Fat' ||
-          nutrients[i].name === 'Carbohydrates' || nutrients[i].name === 'Protein' ||
-          nutrients[i].name === 'Fiber' || nutrients[i].name === 'Sugar' ||
-          nutrients[i].name === 'Sodium' ) {
-              nutArr.push(nutrients[i])
-          }
-      }
-
+      console.log(recipe);
 
   return (
     <div className="single-container">
@@ -53,14 +44,45 @@ const SingleRecipe = ({getRecipe, recipe}) => {
                 <span className="servings">Serves: {servings}</span>
                 <span className="ready-time">Ready In: {readyInMinutes} minutes</span>
             </div>
-            <div className="nutrition">
-            {nutrition && nutArr.map(nutrient => (
-              <div className="nutritional-info">
-                {nutrient.name ==="Carbohydrates" ? 
-                <span className='nutrient nutrient-name'>Carbs</span>
-                  : <span className='nutrient nutrient-name'>{nutrient.name}</span>}
-                <span className='nutrient nutrient-amount'>{nutrient.amount}{nutrient.unit}</span>
+            <div className="types-container">
+              <div className="type dish-type"> 
+                <div className="type-heading dishes">Dish Type: </div>
+                  {dishTypes && dishTypes.length > 0 ? dishTypes.map(dish =>(
+                      <div className="type">{dish}</div>
+                  )) : <div className="type">Info not available</div>}
               </div>
+              <div className="type-heading cuisine-type"> 
+                <div className="cuisines">Cuisine: </div>
+                  {cuisines && cuisines.length >0 ? cuisines.map(cuisine =>(
+                      <div className="cuitype">{cuisine}</div>
+                  )) : <div className="type">Info not available</div>}
+              </div>
+              <div className="type-heading diet-type"> 
+                <div className="special-diets">Diets: </div>
+                  {diets && diets.length>0 ? diets.map(diet =>(
+                        <div className="type">{diet}</div>
+                  )) : <div className="type">Info not available</div>}
+              </div>
+            </div>
+            <div className="nutrition">
+            {nutrition && nutrition.nutrients.map(nutrient => (
+              <Fragment>
+              {/*Selecting specific nutrients by name and mapping them to screen*/}
+                {nutrient.name ==="Carbohydrates" || nutrient.name ==="Protein" ||
+                nutrient.name ==="Fibre" || nutrient.name ==="Calories" ||
+                nutrient.name ==="Fat" || nutrient.name ==="Sugar" || nutrient.name ==="Sodium" ? 
+                <div className="nutritional-info" >
+                {/*Pullin out carbohydrates and shortening name to 'Carbs' for display purposes*/}
+                {nutrient.name==="Carbohydrates" ? <div className='nutrient nutrient-name'>Carbs</div> 
+                : <div className='nutrient nutrient-name'>{nutrient.name}</div>
+                }
+                <div className='nutrient nutrient-amount'>
+                {nutrient.amount}
+                <span className="unit">{nutrient.unit}</span>
+                </div>
+                </div>
+                : null}
+              </Fragment>
             ))}
             </div>
           </div>
@@ -72,8 +94,8 @@ const SingleRecipe = ({getRecipe, recipe}) => {
         <div className="ingredients">
             <h4>Ingredients</h4>
             <ul>
-              {extendedIngredients && extendedIngredients.map(ingredient => (
-                <li key={ingredient.id}>{ingredient.original}</li>
+              {extendedIngredients && extendedIngredients.map((ingredient, index) => (
+                <li key={ingredient.id+index}>{ingredient.original}</li>
               ))}
             </ul> 
           </div>
